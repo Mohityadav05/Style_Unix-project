824afc4cfa1a1394b408ededf554d35bb61b98e3
Mohit Yadav <my4056439@gmail.com>
2026-02-08 10:27:40 +0530

    debug(deploy): robust routing and api test endpoint

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
 const app = require('../backend/server');
+
+// Test route to verify function is reached
+app.get('/api', (req, res) => res.json({ message: "API is alive", env: process.env.NODE_ENV }));
+
 module.exports = app;
+
